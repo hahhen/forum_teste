896702f95f62bb47034c581965c71c07baf913da
hahhen <arthurmp2007@hotmail.com>
2023-08-16 04:32:25 -0300

    Finished CKEditor + responsivity on 'criar' page

---
+++ ckeditor/build/ckeditor.d.ts
@@ -0,0 +1,44 @@
+/**
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import { ClassicEditor } from '@ckeditor/ckeditor5-editor-classic';
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
+import { Bold, Code, Italic, Subscript, Superscript, Underline } from '@ckeditor/ckeditor5-basic-styles';
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
+import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
+import { Essentials } from '@ckeditor/ckeditor5-essentials';
+import { FontSize } from '@ckeditor/ckeditor5-font';
+import { Heading } from '@ckeditor/ckeditor5-heading';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import { AutoImage, Image, ImageCaption, ImageInsert, ImageResize, ImageStyle, ImageToolbar, ImageUpload } from '@ckeditor/ckeditor5-image';
+import { Indent } from '@ckeditor/ckeditor5-indent';
+import { TextPartLanguage } from '@ckeditor/ckeditor5-language';
+import { AutoLink, Link } from '@ckeditor/ckeditor5-link';
+import { List } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
+import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
+import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
+import { SpecialCharacters, SpecialCharactersEssentials, SpecialCharactersLatin, SpecialCharactersMathematical } from '@ckeditor/ckeditor5-special-characters';
+import { Table, TableCaption, TableCellProperties, TableColumnResize, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { TextTransformation } from '@ckeditor/ckeditor5-typing';
+import { WordCount } from '@ckeditor/ckeditor5-word-count';
+declare class Editor extends ClassicEditor {
+    static builtinPlugins: (typeof Alignment | typeof TextTransformation | typeof Autoformat | typeof Superscript | typeof Subscript | typeof Bold | typeof Code | typeof Italic | typeof Underline | typeof BlockQuote | typeof CloudServices | typeof CodeBlock | typeof Essentials | typeof FontSize | typeof Paragraph | typeof Heading | typeof HorizontalLine | typeof AutoImage | typeof Image | typeof ImageCaption | typeof ImageInsert | typeof ImageResize | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof Indent | typeof TextPartLanguage | typeof AutoLink | typeof Link | typeof List | typeof MediaEmbed | typeof PasteFromOffice | typeof RemoveFormat | typeof SpecialCharacters | typeof SpecialCharactersEssentials | typeof SpecialCharactersLatin | typeof SpecialCharactersMathematical | typeof Table | typeof TableCaption | typeof TableCellProperties | typeof TableColumnResize | typeof TableToolbar | typeof WordCount)[];
+    static defaultConfig: {
+        toolbar: {
+            items: string[];
+        };
+        language: string;
+        image: {
+            toolbar: string[];
+        };
+        table: {
+            contentToolbar: string[];
+        };
+    };
+}
+export default Editor;
--- src/pages/Criar.js
+++ src/pages/Criar.js
@@ -2,39 +2,40 @@ import React from 'react'
 import Header from '../components/header';
 import './criar.css'
 import { isLight } from '../App';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
+import { CKEditor } from '@ckeditor/ckeditor5-react'
 
 function Criar() {
     return (
         <>
             <Header />
-            <main className='container p-5'>
-                <h1 className='create-header'>Criar tópico</h1>
-                <div className='options-wrapper'>
-                    <div className='wrapper-select'>
-                        <span>Seção: </span>
-                        <select className='section-select form-select ps-1'>
+            <main className='container pt-5'>
+                <h1 className='create-header mb-3'>Criar tópico</h1>
+                <div className='options-wrapper mb-4'>
+                    <div className='wrapper-select mb-2'>
+                        <label for='section-select'>Seção: </label>
+                        <select id='section-select' className='section-select form-select ps-1'>
                             <option>Matemática</option>
                             <option>Português</option>
                             <option>Desenvolvimento de Sistemas</option>
                         </select>
                     </div>
-                    <div className='wrapper-select'>
-                        <span>Privacidade: </span>
-                        <select className='section-select form-select ps-1'>
+                    <div className='wrapper-select mb-2'>
+                        <label for='privacy-select'>Privacidade: </label>
+                        <select id='privacy-select' className='section-select form-select ps-1'>
                             <option>Privado</option>
                             <option>Minha instituição</option>
                             <option selected>Público</option>
                         </select>
                     </div>
-                    <div className=''>
-                        <button className='post-button m-0 btn btn-primary'>Criar tópico</button>
-                    </div>
                 </div>
+                <div className='title-wrapper mb-4'>
+                    <label for='topicTitle' className='mb-2'>Título <small>(obrigatório)</small></label>
+                    <input type='text' className='topic-title' name='topicTitle' id='topicTitle' />
+                </div>
+                <label className='mb-2'>Corpo:</label>
                 <CKEditor
-                    editor={ClassicEditor}
+                    editor={Editor}
                     data="<p>Escreva aqui seu tópico!</p>"
                     onReady={editor => {
                         // You can store the "editor" and use when it is needed.
@@ -51,6 +52,7 @@ function Criar() {
                         console.log('Focus.', editor);
                     }}
                 />
+                <button id='post-button' className='mt-4 mb-5 post-button m-0 btn btn-primary'>Criar tópico</button>
             </main>
         </>
     )
