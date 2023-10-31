430ce1e42691c8683b43fb515a76edaa03d2bb40
TerryKrom <terrykrom1@gmail.com>
2023-10-30 22:23:44 -0300

    dark mode updated (section.js)

--- src/components/section.js
+++ src/components/section.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { section} from './topicInfoSon'
+import { section, supersection} from './topicInfoSon'
 import { isLight } from '../App'
 import { Link } from 'react-router-dom'
 import './section.css'
@@ -78,6 +78,27 @@ function Section() {
                     </ul>
                 </details>
             </div>
+            <div className={`section-wrapper mb-5 ${getModeClass('', 'dark')}`}>
+            <h4 className={`section-top-title`}>
+                Diversos
+            </h4>
+            {supersection.slice(5, supersection.length).map((sinfo) =>
+                <details className={`pb-1 section ${getModeClass('', 'dark')}`}>
+                    <summary className={`section-title mb-3`}>{sinfo.ssectionname}</summary>
+                    <ul className='pt-2 m-0 section-list'>
+                        {section.map((info) =>
+                            info.ssection === sinfo.ssectioncod ?
+                                <li>
+                                    <div className={`mb-2 section-1`}>
+                                        <Link to={`/${info.sectionname}`} className={`subsection-title ${getModeClass('', 'dark')}`}>{info.sectionname}</Link>
+                                    </div>
+                                </li>
+                            : null
+                        )}
+                    </ul>
+                </details>
+            )}
+        </div>
         </>
     )
 }
