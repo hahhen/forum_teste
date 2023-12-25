60db2cd550fd6adc21fbdf086e43244f8fec87d8
hahhen <arthurmp2007@hotmail.com>
2023-12-25 18:54:34 -0300

    opengraph test#1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Feed from '../components/feed';
 import Section from '../components/sectionshomedropdown';
+import { Helmet } from 'react-helmet-async';
 
 function Home() {
     return (
         <>
-            <Feed section1={1}/>
+            <Helmet>
+                {/* Tags de título e descrição normais */}
+                <title>Centro Paula Souza | Fórum</title>
+                <meta name="description" content="Conecte-se com estudantes e professores, compartilhe conhecimento, tire dúvidas e participe de discussões sobre educação, tecnologia e inovação." />
+            </Helmet>
+            <Feed section1={1} />
             <Section />
         </>
     )
